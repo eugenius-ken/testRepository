40ba1c00997a93793c59c4807bd0ff29bce25f7c
Evgeniy Konyukov <Eugene_Ken@hotmail.com>
2017-07-10 22:35:29 +0500

    change structure

--- src/app/lk/lk.component.ts
+++ src/app/lk/lk.component.ts
@@ -2,6 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'lk-page',
-    templateUrl: './lk.component.html'
+    templateUrl: './lk.component.html',
+    styles: [`
+        .container-fluid { 
+            background-color: #d8d8d8;
+            height: 100%;
+            padding-left: 0px;
+            padding-right: 0px;
+        }
+        .container {
+            padding-top: 20px;
+        }
+    `]
 })
 export class LkComponent { }
