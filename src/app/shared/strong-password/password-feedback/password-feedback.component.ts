79b6824cc63e7da14914eba15de489311c91976c
eugenius-ken <eugene_ken@hotmail.com>
2017-08-02 12:31:24 +0500

    fix problem with build prod

--- src/app/shared/strong-password/password-feedback/password-feedback.component.ts
+++ src/app/shared/strong-password/password-feedback/password-feedback.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class PasswordFeedbackComponent implements OnInit {
 
   @Input()
-  feedback = {};
+  feedback = <any>{};
 
 
   constructor() { }
